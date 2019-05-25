15679f705fe6eed473377fe2c38e90dc1119bd55
brandon <brandon.burton@thebeansgroup.com>
2019-05-25 21:39:54 +0100

    Add test for handling bad URIs

--- spec/lib/crawler_spec.rb
+++ spec/lib/crawler_spec.rb
@@ -5,9 +5,10 @@ require './lib/crawler'
 
 RSpec.describe Crawler do
   describe '.crawl_internal_links' do
-    let(:domain) { 'https://monzo.com' }
-    let(:html_body) do
-      <<-HTML
+    context 'when requesting good URIs' do
+      let(:domain) { 'https://monzo.com' }
+      let(:html_body) do
+        <<-HTML
         <html>
           <head><title>Monzo - The Bank of the Future</title></head>
           <body>
@@ -33,40 +34,51 @@ RSpec.describe Crawler do
             </div>
           </footer>
         </html
-      HTML
-    end
+        HTML
+      end
 
-    let(:expected_links) do
-      %w[
-        /about
-        /blog
-        /community
-        /help
-      ]
-    end
+      let(:expected_links) do
+        %w[
+          /about
+          /blog
+          /community
+          /help
+        ]
+      end
 
-    let(:unexpected_links) do
-      %w[
-        /
-        https://twitter.com
-        https://facebook.com
-        https://instagram.com
-      ]
-    end
+      let(:unexpected_links) do
+        %w[
+          /
+          https://twitter.com
+          https://facebook.com
+          https://instagram.com
+        ]
+      end
 
-    before do
-      expect(described_class).to receive(:open).and_return(html_body)
-    end
+      before do
+        expect(described_class).to receive(:open).and_return(html_body)
+      end
 
-    it 'returns internal links for a given domain' do
-      retrieved_links = described_class.crawl_internal_links(domain)
+      it 'returns internal links for a given domain' do
+        retrieved_links = described_class.crawl_internal_links(domain)
 
-      aggregate_failures do
-        unexpected_links.each do |bad_link|
-          expect(retrieved_links).not_to include(bad_link)
+        aggregate_failures do
+          unexpected_links.each do |bad_link|
+            expect(retrieved_links).not_to include(bad_link)
+          end
+
+          expect(retrieved_links).to match_array(expected_links)
         end
+      end
+    end
 
-        expect(retrieved_links).to match_array(expected_links)
+    context 'when requesting bad URIs' do
+      let(:bad_domain) { 'https://monzo.com/not-a-page' }
+
+      it 'returns an error message' do
+        expect { described_class.crawl_internal_links(bad_domain) }
+          .to output("Couldn't retrieve links from #{bad_domain}\n")
+          .to_stdout
       end
     end
   end
