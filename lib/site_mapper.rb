c91f371ae5be29e53211043e55847ab67fab08ff
brandon <brandon.burton@thebeansgroup.com>
2019-05-25 19:00:41 +0100

    Nest first links under base url
    When we are building the site map we need to
    start by nesting the links under the base url

--- lib/site_mapper.rb
+++ lib/site_mapper.rb
@@ -1,15 +1,59 @@
 # frozen_string_literal: true
 
+require 'set'
+
 class SiteMapper
   attr_reader :domain
-  attr_accessor :links
+  attr_accessor :site_map, :encountered_paths
 
   def initialize(domain)
     @domain = domain
-    @links = [domain]
+    @site_map = { "#{domain}": {} }
+    @encountered_paths = Set.new
   end
 
-  def retrieve_internal_links
-    links + Crawler.crawl_internal_links(domain)
+  def map_site
+    retrieve_internal_links([domain])
+  end
+
+  def retrieve_internal_links(paths)
+    retrieved_paths = nil
+
+    paths.each do |path|
+      full_path = build_path(path)
+
+      retrieved_paths = Crawler.crawl_internal_links(full_path)
+      add_to_site_map(retrieved_paths)
+    end
+
+    unless retrieved_paths_mapped_already(retrieved_paths)
+      retrieve_internal_links(retrieved_paths)
+    end
+
+    site_map
+  end
+
+  private
+
+  def build_path(path)
+    return domain if path == domain
+
+    domain + path
+  end
+
+  def add_to_site_map(paths)
+    paths.each do |path|
+      next if encountered_paths.include?(path)
+
+      site_map[domain.to_sym][path.to_sym] = {}
+    end
+
+    encountered_paths.merge(paths)
+  end
+
+  def retrieved_paths_mapped_already(paths)
+    paths.any? do |path|
+      !site_map.keys.include?(path)
+    end
   end
 end
--- spec/lib/site_mapper_spec.rb
+++ spec/lib/site_mapper_spec.rb
@@ -8,7 +8,9 @@ RSpec.describe SiteMapper do
   describe '#retrieve_internal_links' do
     let(:domain) { 'https://monzo.com' }
 
-    let(:crawled_links) do
+    subject { described_class.new(domain) }
+
+    let(:first_links) do
       %w[
         /about
         /blog
@@ -17,18 +19,26 @@ RSpec.describe SiteMapper do
       ]
     end
 
-    let(:expected_links) do
-      [domain] + crawled_links
-    end
+    context 'no nested links' do
+      let(:expected_links) do
+        {
+          "#{domain}": {
+            "/about": {},
+            "/blog": {},
+            "/community": {},
+            "/help": {}
+          }
+        }
+      end
 
-    subject { described_class.new(domain) }
+      before do
+        allow(Crawler).to receive(:crawl_internal_links)
+          .and_return(first_links)
+      end
 
-    before do
-      allow(Crawler).to receive(:crawl_internal_links).and_return(crawled_links)
-    end
-
-    it 'returns internal links for a given domain' do
-      expect(subject.retrieve_internal_links).to eq(expected_links)
+      it 'returns internal links for a given domain' do
+        expect(subject.map_site).to eq(expected_links)
+      end
     end
   end
 end
